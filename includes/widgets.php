df9dd7b62bff8e7c2a8d5ccf9af99697320f801f
Keith Solomon <keith@solowebdeisgns.net>
2020-09-15 16:09:57 -0500

    Change(footer): Footer columns and widgets Set up footer for 3 columns, add widget areas to columns

--- includes/widgets.php
+++ includes/widgets.php
@@ -1,13 +1,43 @@
 <?php
 /*
 	Solo-Frame Widgets Functions
-	
+
 	This file is a part of the Solo-Frame WordPress theme framework.
 */
 
 function sf_register_widgets() {
 	register_sidebar(array(
 		'name'=>'Primary Sidebar',
+		'id' => 'primary',
+		'description' => 'Sidebar widget area for regular pages.',
+		'before_widget' => '<div class="widgetblock %2$s" id="%1$s" >',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widgettitle">',
+		'after_title' => '</h3>',
+	));
+
+	register_sidebar(array(
+		'name' => 'Footer Column 1',
+		'id' => 'primary',
+		'description' => 'Sidebar widget area for regular pages.',
+		'before_widget' => '<div class="widgetblock %2$s" id="%1$s" >',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widgettitle">',
+		'after_title' => '</h3>',
+	));
+
+	register_sidebar(array(
+		'name' => 'Footer Column 2',
+		'id' => 'primary',
+		'description' => 'Sidebar widget area for regular pages.',
+		'before_widget' => '<div class="widgetblock %2$s" id="%1$s" >',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widgettitle">',
+		'after_title' => '</h3>',
+	));
+
+	register_sidebar(array(
+		'name' => 'Footer Column 3',
 		'id' => 'primary',
 		'description' => 'Sidebar widget area for regular pages.',
 		'before_widget' => '<div class="widgetblock %2$s" id="%1$s" >',
--- sidebar.php
+++ sidebar.php
@@ -1,41 +1,7 @@
 <!-- sidebar -->
 <aside id="sidebar" class="sidebar">
 	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Primary Sidebar')) : else : ?>
-		<section class="widgetblock" id="search">
-			<h3 class="widgettitle">Site Search</h3>
 
-			<?php get_search_form(); ?>
-		</section>
-
-		<section class="widgetblock" id="pages">
-			<h3 class="widgettitle">Pages</h3>
-
-			<ul>
-				<?php wp_list_pages('title_li='); ?>
-			</ul>
-		</section>
-
-		<section class="widgetblock" id="categories">
-			<h3 class="widgettitle">Categories</h3>
-
-			<ul>
-				<?php wp_list_categories('title_li='); ?>
-			</ul>
-		</section>
-
-		<section class="widgetblock" id="archives">
-			<h3 class="widgettitle">Archives</h3>
-
-			<ul>
-				<?php wp_get_archives('type=monthly&limit=12&show_post_count=1'); ?>
-			</ul>
-		</section>
-
-		<section class="widgetblock" id="calendar">
-			<h3 class="widgettitle">Calendar</h3>
-
-			<?php get_calendar(); ?>
-		</section>
 	<?php endif; ?>
 </aside>
 <!-- /sidebar -->
