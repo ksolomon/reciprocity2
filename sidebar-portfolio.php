ac107732338a1a3afd0add642d6e7ec555903976
Keith Solomon <keith@solowebdesigns.net>
2020-09-18 11:01:52 -0500

    UPdated portfolio sidebar to scroll with content

--- sidebar-portfolio.php
+++ sidebar-portfolio.php
@@ -1,5 +1,5 @@
 <!-- sidebar -->
-<aside id="sidebar" class="sidebar">
+<aside id="sidebar" class="sidebar projects">
 	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Portfolio Sidebar')) : else : ?>
 
 	<?php endif; ?>
